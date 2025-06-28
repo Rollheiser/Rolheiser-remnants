5ece1e016dd3dc424a224b41d9d2200c6e89ee0e
Rollheiser <142363345+Rollheiser@users.noreply.github.com>
2025-06-28 15:19:18 -0500

    Create user.py

---
+++ Projects/To-do List/user.py
@@ -0,0 +1,8 @@
+""" Here is the class for the user, essential to create more customization and 
+    create and save your tasks """
+
+class User:
+    def __init__(self, name: str, password: str, email: str):
+        self.name_user = name
+        self.password_account = password
+        self.email_account = email
